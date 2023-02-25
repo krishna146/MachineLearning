f99de62b485a659f9c854f27fe182d23d1da48d1
krishna kumar <krishnakumarjms12@gmail.com>
2023-02-25 18:16:06 +0530

    Image Labeling/Object detection code added

--- app/src/main/java/com/example/machinelearning/MainActivity.kt
+++ app/src/main/java/com/example/machinelearning/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.example.machinelearning
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.machinelearning.databinding.ActivityMainBinding
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeler
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+    private lateinit var inputImage: InputImage
+    lateinit var imageLabeler: ImageLabeler
+    private val contract = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        val photo = MediaStore.Images.Media.getBitmap(this.getContentResolver(), it);
+        binding.imgSelected.setImageBitmap(photo)
+        inputImage = InputImage.fromBitmap(photo, 0)
+        processImage()
+    }
+
+    private fun processImage() {
+        imageLabeler.process(inputImage).addOnSuccessListener { labels ->
+            var result = ""
+            for (label in labels) {
+                result = result + "\n" + label.text
+            }
+            binding.txtResult.text = result
+        }.addOnFailureListener {
+
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         // This is a comment.. -- Aman Kumar
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        imageLabeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
+        setContentView(binding.root)
+        binding.btnChoosePicture.setOnClickListener {
+            contract.launch("image/*")
+        }
+
     }
+
 }
